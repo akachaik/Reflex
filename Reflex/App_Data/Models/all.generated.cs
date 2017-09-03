4dca4319c51c0040b46bc7a81c15ea291165e5c8
AkachaiPC <AkachaiPC@DESKTOP-7KMMA7G>
2017-09-03 23:56:58 +0700

    Add contact form

--- Reflex/App_Data/Models/all.generated.cs
+++ Reflex/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "92ca7f44bdfb51a6")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
---
+++ Reflex/Controllers/ContactController.cs
@@ -0,0 +1,70 @@
+﻿using System.Web.Mvc;
+using Reflex.Models;
+using Umbraco.Web.Mvc;
+using System.Net.Mail;
+using System.Web.Configuration;
+using log4net;
+using System.Reflection;
+
+namespace Reflex.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            return PartialView(GetViewPath("_ContactForm"), new ContactViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView(GetViewPath("_ContactForm"), model);
+        }
+
+        [HttpPost]
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            var success = false;
+            if (ModelState.IsValid)
+            {
+                success = SendEmail(model);
+            }
+
+            return PartialView(GetViewPath(success ? "_Success" : "_Error"));
+        }
+
+        public bool SendEmail(ContactViewModel model)
+        {
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            try
+            {
+                var message = new MailMessage();
+                var client = new SmtpClient();
+
+                var toAddress = WebConfigurationManager.AppSettings["ContactEmailTo"];
+                var fromAddress = WebConfigurationManager.AppSettings["ContactEmailFrom"];
+
+                message.Subject = $"Enquiry from: {model.Name} - {model.Email}";
+                message.Body = model.Message;
+                message.To.Add(new MailAddress(toAddress, toAddress));
+                message.From = new MailAddress(fromAddress, fromAddress);
+
+                client.Send(message);
+                return true;
+
+            }
+            catch (System.Exception ex)
+            {
+                log.Error("Contact Form Error", ex);
+                return false;
+            }
+        }
+
+        private string GetViewPath(string name)
+        {
+            return $"/Views/Partials/Contact/{name}.cshtml";
+        }
+    }
+}
